60ecd75af6630f1a3bd9c84d2fd84360fee446ff
TavaresDylan <tavares-dylan03@gmail.com>
2019-04-08 16:35:12 +0200

    Bootstrap add

--- index.php
+++ index.php
@@ -3,47 +3,78 @@
 <head>
 	<title>Php Get</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	 <link href="form-validation.css" rel="stylesheet">
+	 <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/checkout/">
 </head>
 <body>
 
+   
+<div class="container-fluid bg-primary">
 
-	<form method="Get">
-		<label>Nom  :</label>
-			<input type="text" name="nom" required="true" value="Votre nom*">
-		</br />
-		<label>Prénom :</label>
-			<input type="text" name="prenom" required="true" value="Votre prénom *">
-		</br />
-		<label>N° de Rue :</label>
-			<input type="number" name="num" value="Votre numéro de rue">
-		</br />
-		<label>Type de voie :</label>
-			<input type="text" name="voie" value="Votre type de voie">
-		</br />
-		<label>Nom de rue :</label>
-			<input type="text" name="rue" required="true" value="Votre rue*">
-		</br />
-		<label>Code postal :</label>
-			<input type="number" name="cp" required="true" value="Votre code postal*">
-		</br />
-		<label>Ville</label>
-			<input type="text" name="ville" required="true" value="Votre ville">
-		</br />		
-				<input type="submit">Envoi</input>
-	</form>
+	<form method="Get"> 
+		<div class="form-row">
+
+        	<div class="col-md-6 mb-3 mt-3">
+				<label>Nom  :</label>
+					<input type="text" name="nom" required="true" value="Votre nom*" >
+				</br />
+			</div>
+
+			<div class="col-md-6 mb-3 mt-3">
+				<label>Prénom :</label>
+					<input type="text" name="prenom" required="true" value="Votre prénom *">
+				</br />
+			</div>
+
+			<div class="col-md-6 mb-3">
+				<label>N° de Rue :</label>
+					<input type="number" name="num" value="Votre numéro de rue">
+				</br />
+			</div>
+
+			<div class="col-md-6 mb-3">
+				<label>Type de voie :</label>
+					<input type="text" name="voie" value="Votre type de voie">
+				</br />
+			</div>
+
+			<div class="col-md-6 mb-3">
+				<label>Nom de rue :</label>
+					<input type="text" name="rue" required="true" value="Votre rue*">
+				</br />
+			</div>
+
+			<div class="col-md-6 mb-3">
+				<label>Code postal :</label>
+					<input type="number" name="cp" required="true" value="Votre code postal*">
+				</br />
+			</div>
+
+			<div class="col-md-6 mb-3">
+				<label>Ville</label>
+					<input type="text" name="ville" required="true" value="Votre ville">
+				</br />
+			</div>
+
+						<input class="btn btn-outline-danger btn-lg btn-block" type="submit"></input>
+		</form>
+	</div>
+
 <?php
 	//var_dump($_GET);
-if (isset($GET_['nom']) && isset($_GET['prenom']) && isset($_GET['rue']) && isset($_GET['cp'])&& isset($_GET['ville'])) {
+if (isset($GET_['nom']) &&
+    isset($_GET['prenom']) && 
+    isset($_GET['rue']) && 
+    isset($_GET['cp'])&& 
+    isset($_GET['ville'])) {
 	echo 'Bonjour'.$_GET['nom'].''.$_GET['prenom'].' vous habitez le '.$_GET['rue'].''.$_GET['cp'].' à '.$_GET['ville'];
 }
 var_dump($_GET);
-echo 'Bonjour '.$_GET['nom'].' '.$_GET['prenom'].' vous habitez le '.$_GET['num'].' '.$_GET['voie'].' '.$_GET['rue'].' '.$_GET['cp'].' à '.$_GET['ville']
-
-
+echo 'Bonjour '.ucfirst($_GET['nom']).' '.ucfirst($_GET['prenom']).' vous habitez le '.$_GET['num'].' '.ucfirst($_GET['voie']).' '.ucfirst($_GET['rue']).' '.$_GET['cp'].' à '.ucfirst($_GET['ville']);
 
 ?>
-
-
 
 </body>
 </html>
